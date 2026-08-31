9eace016ebd5b8f5869862afb2a4fff88a6ef55c
shizukamoon <shizukatsuki865@gmail.com>
2026-08-31 09:04:41 +0700

    Add files via upload

---
+++ moredata.js
@@ -0,0 +1,98 @@
+const modelProfiles = {
+    "穂高ひな Hina Hodaka": {
+        photo: "img/av/穂高ひな Hina Hodaka.webp",
+        bio: "DOB: 2001-10-30 Measurements: 79-57-83 - Cup: G - Height: 156 cm"
+    },
+    "名波ルナ Nanami Luna": {
+        photo: "img/av/名波ルナ Nanami Luna.webp",
+        bio: ""
+    },
+    "天緒まい Mai Amao": {
+        photo: "img/av/天緒まい Mai Amao.webp",
+        bio: ""
+    },
+    "丘咲エミリ Emiri Okazaki": {
+        photo: "img/av/丘咲エミリ Emiri Okazaki.webp",
+        bio: "DOB: 1991-04-10 Measurements: 87-56-87 - Cup: D - Height: 165 cm"
+    },
+    "nana_taipei": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "Chloe": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "iam_sostrong": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "艾熙": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "懒懒猪": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "萌宅卡": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "苏小涵": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "Misa酱": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "菠萝啤beer": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "梓怡学妹": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "椰椰小猫": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "酒酱": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "斑斑": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "Livecam": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "PuppyPorn090": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "BJ徐婉": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "青山茉悠 Aoyama Mayu": {
+        photo: "img/av/青山茉悠 Aoyama Mayu.webp",
+        bio: ""
+    },
+    "木村美羽 Miu Kimura": {
+        photo: "img/av/木村美羽 Miu Kimura.webp",
+        bio: ""
+    },
+    "川田みり Miri Kawada": {
+        photo: "img/av/.webp",
+        bio: ""
+    },
+    "希望みりな Nozomi Mirina": {
+        photo: "img/av/.webp",
+        bio: ""
+    }
+};
